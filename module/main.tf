76fadbfb5d052e6b7f61443c89b073ee07c75451
EC2 Default User <ec2-user@ip-172-31-82-218.ec2.internal>
2022-02-04 19:24:04 +0000

    added module

--- module/main.tf
+++ module/main.tf
@@ -1,3 +1,7 @@
-# module "vpc" {
-#     source = "../"
+module "project1_vpc" {
+    source = "../"
+    region = "us-east-1"
+}
+# output region {
+#     value = module.project1_vpc.region
 # }
--- variable.tf
+++ variable.tf
@@ -1,6 +1,9 @@
 variable "region" {
+  type = string
+  description = "Region where we create a resource"
   default = "us-east-1"
 }
+
 
 
 
