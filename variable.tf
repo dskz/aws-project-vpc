f021efda1d0bd067bcd99ee996a51281c8fdea56
dskz <dilna3ka@gmail.com>
2022-02-18 21:50:20 +0000

    changed file structure

--- variable.tf
+++ variable.tf
@@ -1,6 +1,6 @@
 variable "region" {
   type = string
-  description = "Region where we create a resource"
+  description = "Region where we create a VPC"
   default = "us-east-1"
 }
 
--- vpc.tf
+++ vpc.tf
@@ -3,3 +3,125 @@ resource "aws_vpc" "project1" {
   tags       = local.common_tags
 
 }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.project1.id
+  tags   = local.common_tags
+}
+
+resource "aws_eip" "nat" {
+  vpc  = true
+  tags = local.common_tags
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public1.id
+  tags          = local.common_tags
+}
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.project1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = local.common_tags
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public3" {
+  subnet_id      = aws_subnet.public3.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.project1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gw.id
+  }
+  tags = local.common_tags
+}
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private3" {
+  subnet_id      = aws_subnet.private3.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.project1.id
+  cidr_block              = "10.0.101.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "public1"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.project1.id
+  cidr_block              = "10.0.102.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+  tags = {
+    Name = "public2"
+  }
+}
+
+resource "aws_subnet" "public3" {
+  vpc_id                  = aws_vpc.project1.id
+  cidr_block              = "10.0.103.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1c"
+  tags = {
+    Name = "public3"
+  }
+}
+resource "aws_subnet" "private1" {
+  vpc_id                  = aws_vpc.project1.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "private1"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id                  = aws_vpc.project1.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+  tags = {
+    Name = "private2"
+  }
+}
+
+resource "aws_subnet" "private3" {
+  vpc_id                  = aws_vpc.project1.id
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1c"
+  tags = {
+    Name = "private3"
+  }
+}
